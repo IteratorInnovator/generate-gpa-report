aba1ea7a943822ed150eda9e6cce1c3566a66fa2
IteratorInnovator <harryngkokjing@gmail.com>
2025-01-05 23:17:36 +0800

    Implement sem gpa and cgpa calculator functions

--- process_info.py
+++ process_info.py
@@ -2,6 +2,7 @@
 
 #!/usr/bin/env python3
 
+from decimal import Decimal
 import json
 from reportlab.platypus import Table
 from reportlab.pdfgen import canvas
@@ -25,18 +26,38 @@ def load_modules(json_file):
     with open(json_file,'r') as file:
         return json.load(file)
 
-def calculate_semester_gpa(current_sem_modules):
-    total_sem_grade_points = []
-    total_sem_credit_units = []
+def calculate_semester_total(current_sem_modules):
+    list_of_sem_grade_points = []
+    list_of_sem_credit_units = []
     for module in current_sem_modules:
         grade = module["grade"]
         if grade in grade_to_points:
-            total_sem_grade_points.append(grade_to_points[grade])
-            total_sem_credit_units.append(module["credit_unit"])
-    return sum(total_sem_grade_points) / sum(total_sem_credit_units)
-    
+            list_of_sem_grade_points.append(grade_to_points[grade])
+            list_of_sem_credit_units.append(module["credit_unit"])
+    total_sem_grade_points = sum(list_of_sem_grade_points)
+    total_sem_credit_units = sum(list_of_sem_credit_units)
+    semester_gpa = total_sem_grade_points / total_sem_credit_units
+    return semester_gpa, total_sem_credit_units
+
+# Take note returned value is a tuple consisting of semester gpa and total credit units of str type
+
+
+def calculate_cumulative_total(list_of_semester_totals):
+    cumulative_total_grade_points = 0
+    cumulative_total_credits = 0
+    for semester in list_of_semester_totals:
+        cumulative_total_grade_points += semester[0] * semester[1]
+        cumulative_total_credits += semester[1]
+    cummulative_gpa = round(cumulative_total_grade_points / cumulative_total_credits, 2)
+    return cummulative_gpa, cumulative_total_credits
+
+# Take note returned value is a tuple consisting of cumulative gpa and total credit units of str type  
+
+
 def generate_table_data(current_sem_modules):
     pass
 
-sem = load_modules("modules.json")
-print(calculate_semester_gpa(sem[0]["modules"]))
+
+sem = load_modules("semesters_data.json")
+sems = [calculate_semester_total(module["modules"]) for module in sem]
+print(calculate_cumulative_total(sems))
