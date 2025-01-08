91bcb66a0866ff15cc3efe4c6a91c3f348bb83c2
IteratorInnovator <harryngkokjing@gmail.com>
2025-01-08 12:41:29 +0800

    Implement unit test case for calc sem total functions

--- process_data.py
+++ process_data.py
@@ -2,21 +2,23 @@
 
 #!/usr/bin/env python3
 
+from decimal import Decimal
+import decimal
 import json
 
 grade_to_points = {
-    "A+": 4.3,  # Highest grade
-    "A": 4.0,
-    "A-": 3.7,
-    "B+": 3.3,
-    "B": 3.0,
-    "B-": 2.7,
-    "C+": 2.3,
-    "C": 2.0,
-    "C-": 1.7,
-    "D+": 1.3,
-    "D": 1.0,
-    "F": 0.0,  # Fail
+    "A+": Decimal('4.3'),  # Highest grade
+    "A": Decimal('4.0'),
+    "A-": Decimal('3.7'),
+    "B+": Decimal('3.3'),
+    "B": Decimal('3.0'),
+    "B-": Decimal('2.7'),
+    "C+": Decimal('2.3'),
+    "C": Decimal('2.0'),
+    "C-": Decimal('1.7'),
+    "D+": Decimal('1.3'),
+    "D": Decimal('1.0'),
+    "F": Decimal('0.0'),  # Fail
 }
 
 
@@ -32,11 +34,14 @@ def calculate_semester_total(current_sem_modules):
         grade = module["grade"]
         if grade in grade_to_points:
             list_of_sem_grade_points.append(grade_to_points[grade])
-            list_of_sem_credit_units.append(float(module["credit_unit"]))
+            list_of_sem_credit_units.append(Decimal(module["credit_unit"]))
     total_sem_grade_points = sum(list_of_sem_grade_points)
     total_sem_credit_units = sum(list_of_sem_credit_units)
-    semester_gpa = total_sem_grade_points / total_sem_credit_units
-    return semester_gpa, total_sem_credit_units
+    try:
+        semester_gpa = total_sem_grade_points / total_sem_credit_units
+    except ZeroDivisionError as e:
+        return (0.0, 0.0)
+    return float(semester_gpa), float(total_sem_credit_units)
 
 
 def calculate_cumulative_total(list_of_semester_totals):
@@ -45,8 +50,11 @@ def calculate_cumulative_total(list_of_semester_totals):
     for semester in list_of_semester_totals: 
         cumulative_total_grade_points += semester[0] * semester[1]
         cumulative_total_credits += semester[1]
-    cummulative_gpa = round(cumulative_total_grade_points / cumulative_total_credits, 2)
-    return cummulative_gpa, cumulative_total_credits
+    try:
+        cummulative_gpa = round(cumulative_total_grade_points / cumulative_total_credits, 2)
+    except ZeroDivisionError as e:
+        return ((0.0, 0.0))
+    return cumulative_total_grade_points, cumulative_total_credits
 
 
 def generate_table_data(current_sem_modules):
@@ -55,9 +63,4 @@ def generate_table_data(current_sem_modules):
     table_data.insert(0,["Course","Grade","Credit Unit(s) Earned","Status"])
     return table_data
 
-
-
-sem = load_modules("semesters_data.json")
-# print(sem)
-# print(calculate_cumulative_total([calculate_semester_total(sem[0]["modules"])]))
-print(generate_table_data(sem[0]["modules"]))
+print(type(sum([Decimal('3.0'), Decimal('3.5')])/Decimal('2.0')))
---
+++ unit_test.py
@@ -0,0 +1,129 @@
+import unittest
+import process_data as pd
+
+class UnitTest_process_data(unittest.TestCase):
+    def setUp(self):
+        return super().setUp()
+    def test_calculate_semester_total(self):
+        testcases = [[
+            {
+                "module": "Computational Thinking and Programming",
+                "grade": "A-",
+                "credit_unit": "1.0",
+                "status": "Completed"
+            },
+            {
+                "module": "Computational Fundamentals",
+                "grade": "B+",
+                "credit_unit": "1.0",
+                "status": "Completed"
+            },
+            {
+                "module": "Writing and Reasoning",
+                "grade": "B+",
+                "credit_unit": "1.0",
+                "status": "Completed"
+            },
+            {
+                "module": "Economics and Society",
+                "grade": "B+",
+                "credit_unit": "1.0",
+                "status": "Completed"
+            }
+        ], 
+        [
+            {
+                "module": "Algorithms and Programming",
+                "grade": "A",
+                "credit_unit": "1.0",
+                "status": "Completed"
+            },
+            {
+                "module": "Data Management",
+                "grade": "A-",
+                "credit_unit": "1.0",
+                "status": "Completed"
+            },
+            {
+                "module": "Web Application Development I",
+                "grade": "B+",
+                "credit_unit": "1.0",
+                "status": "Completed"
+            },
+            {
+                "module": "Big Questions - Happiness and Suffering",
+                "grade": "B+",
+                "credit_unit": "1.0",
+                "status": "Completed"
+            },
+            {
+                "module": "Introduction to Statistics",
+                "grade": "A-",
+                "credit_unit": "1.0",
+                "status": "Completed"
+            },
+        ],
+        [
+            {
+                "module": "Enterprise Solution Development",
+                "grade": "C-",
+                "credit_unit": "1.0",
+                "status": "Completed"
+            },
+            {
+                "module": "Enterprise Solution Management",
+                "grade": "D+",
+                "credit_unit": "1.0",
+                "status": "Completed"
+            },
+            {
+                "module": "Web Application Development II",
+                "grade": "B-",
+                "credit_unit": "1.0",
+                "status": "Completed"
+            }
+        ],
+        [
+            {
+                "module": "Management Communication",
+                "grade": "C+",
+                "credit_unit": "0.5",
+                "status": "Completed"
+            },
+            {
+                "module": "Software Project Management",
+                "grade": "B",
+                "credit_unit": "2.0",
+                "status": "Completed"
+            },
+            {
+                "module": "Object-Oriented Programming",
+                "grade": "A+",
+                "credit_unit": "1.5",
+                "status": "Completed"
+            }
+        ],
+        [
+            
+        ]]
+        self.assertEqual(pd.calculate_semester_total(testcases[0]), (3.4, 4.0)) # Basic testcase
+        self.assertEqual(pd.calculate_semester_total(testcases[1]), (3.6, 5.0)) # Basic testcase
+        self.assertEqual(pd.calculate_semester_total(testcases[2]), (1.9, 3.0)) # Lower grade range testcase
+        self.assertEqual(pd.calculate_semester_total(testcases[3]), (2.4, 4.0)) # Varying credit unit testcase
+        self.assertEqual(pd.calculate_semester_total(testcases[4]), (0.0, 0.0)) # Zero modules testcase
+    def test_calculate_cumulative_total(self):
+        testcases = [
+            [ (3.4, 4.0) ],
+            [ (2.7, 5.0), (4.3, 4.0), (3.58, 5.0) ],
+            [ (3.6, 4.0), (3.7, 4.0), (3.66, 5.0), (4.06, 5.0), (2.75, 4.0) ],
+            []
+        ]
+        self.assertEqual(pd.calculate_cumulative_total(testcases[0]), (3.4, 4.0)) # 1 semester testcase
+        self.assertEqual(pd.calculate_cumulative_total(testcases[1]), (3.47, 14.0)) # Mulitple semesters testcase
+        self.assertEqual(pd.calculate_cumulative_total(testcases[2]), (3.58, 22.0)) # Multiple semesters testcase
+        self.assertEqual(pd.calculate_cumulative_total(testcases[3]), (0.0 ,0.0)) # Zero semester testcase
+
+
+if __name__ == "__main__":
+    # print(help(unittest))
+    unittest.main()
